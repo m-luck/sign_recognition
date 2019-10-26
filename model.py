5c385ba4ab8fd92ec20828919a63abf6094723cb
Michael <michael.j.lukiman@gmail.com>
2019-10-26 14:19:47 -0400

    submission 2

--- model.py
+++ model.py
@@ -7,22 +7,21 @@ nclasses = 43 # GTSRB as 43 classes
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=3)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=2)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=8)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=2)
+        self.conv3 = nn.Conv2d(64, 20, kernel_size=2)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(980, 128)
+        self.fc1 = nn.Linear(1600, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, 64)
         self.fc4 = nn.Linear(64, 64)
         self.fc5 = nn.Linear(64, 50)
-        # self.fc4 = nn.Linear(50, 20)
-        # self.fc5 = nn.Linear(20, 50)
         self.finallayer = nn.Linear(50, nclasses)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 980)
+        x = x.view(-1, 1600)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = F.relu(self.fc2(x))
