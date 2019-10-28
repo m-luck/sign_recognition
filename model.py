d389a7ff59542cb440d4f7ee41c16b288f5dd473
Michael <michael.j.lukiman@gmail.com>
2019-10-27 22:19:10 -0400

    regurg model try

--- model.py
+++ model.py
@@ -8,31 +8,18 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1_1 = nn.Conv2d(1, 64, 5)
-        self.conv2_1 = nn.Conv2d(64, 64, 3)
-        self.conv2_2 = nn.Conv2d(64, 64, 3)
-        self.conv2_3 = nn.Conv2d(64, 64, 3)
-        self.fc1 = nn.Linear(64*7*7 + 64*5*5, 64)
-        self.fc2 = nn.Linear(64, nclasses)
-
-        print(""" \
-            self.conv1_1 = nn.Conv2d(1, 64, 5)
-            self.conv2_1 = nn.Conv2d(64, 64, 3)
-            self.conv2_2 = nn.Conv2d(64, 64, 3)
-            self.conv2_3 = nn.Conv2d(64, 64, 3)
-            self.fc1 = nn.Linear(64*5*5 + 64*1*1, 64)
-            self.fc2 = nn.Linear(64, nclasses)
-              """)
+        self.conv1 = nn.Conv2d(1, 108, 3)
+        self.conv2 = nn.Conv2d(108, 200, 3)
+        self.fc1 = nn.Linear(15 * 15 * 108 + 6 * 6 * 200, 100)
+        self.fc2 = nn.Linear(100, nclasses)
 
     def forward(self, x):
-        x1 = F.max_pool2d(F.relu(self.conv1_1(x)), 4)
-        x2 = F.max_pool2d(F.relu(self.conv2_1(x1)), 3)
-        x2 = F.relu(self.conv2_2(x1))
-        x2 = F.relu(self.conv2_3(x1))
+        x1 = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x2 = F.max_pool2d(F.relu(self.conv2(x1)), 2)
         # print(x1.size())
         # print(x2.size())
-        x1 = x1.view(-1, 64*7*7)
-        x2 = x2.view(-1, 64*5*5)
+        x1 = x1.view(-1, 108*15*15)
+        x2 = x2.view(-1, 200*6*6)
         out = [x1, x2]
         out = torch.cat(out, 1)
         out = F.relu(self.fc1(out))
